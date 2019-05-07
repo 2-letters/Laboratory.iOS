9deecc9315b26476ee53762a2349d245c14b5353
Vo Huy <voxqhuy@gmail.com>
2019-05-07 14:44:09 -0400

    create project

---
+++ Laboratory/ViewModel/LabAssignmentViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LabAssignmentViewModel.swift
+//  Laboratory
+//
+//  Created by Administrator on 5/7/19.
+//  Copyright © 2019 2Letters. All rights reserved.
+//
+
+import Foundation
+
+class LabAssignmentViewModel {
+    
+}
