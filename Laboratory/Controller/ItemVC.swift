e18aa50cc0f1c939372b375c1333b066e2f5441b
Huy Vo <voxqhuy@gmail.com>
2019-05-14 23:57:35 -0400

    add Item View Controller as a tab in the app

---
+++ Laboratory/Controller/ItemVC.swift
@@ -0,0 +1,30 @@
+//
+//  ItemVC.swift
+//  Laboratory
+//
+//  Created by Huy Vo on 5/14/19.
+//  Copyright © 2019 2Letters. All rights reserved.
+//
+
+import UIKit
+
+class ItemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
