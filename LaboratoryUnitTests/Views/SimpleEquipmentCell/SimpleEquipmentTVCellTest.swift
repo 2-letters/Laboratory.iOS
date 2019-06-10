df0c34968eb7872cb17013b848bd0481b798eff7
Vo Huy <voxqhuy@gmail.com>
2019-06-10 15:49:56 -0400

    finish: test all views

---
+++ LaboratoryUnitTests/Views/SimpleEquipmentCell/SimpleEquipmentTVCellTest.swift
@@ -0,0 +1,38 @@
+//
+//  SimpleEquipmentTVCellTest.swift
+//  LaboratoryUnitTests
+//
+//  Created by Developers on 6/10/19.
+//  Copyright © 2019 2Letters. All rights reserved.
+//
+
+import XCTest
+@testable import Laboratory
+
+class SimpleEquipmentTVCellTest: XCTestCase {
+
+    var sut: SimpleEquipmentTVCell!
+    override func setUp() {
+        super.setUp()
+        let bundle = Bundle(for: SimpleEquipmentTVCell.self)
+        let nib = bundle.loadNibNamed(SimpleEquipmentTVCell.nibId, owner: nil, options: nil)
+        sut = nib?.first as? SimpleEquipmentTVCell
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testViewModelDidSet() {
+        // GIVEN
+        let fakeVM = FakeData.simpleEquipmentVM
+        
+        // WHEN
+        sut.viewModel = fakeVM
+        
+        // THEN
+        XCTAssertEqual(sut.equipmentNameLabel.text, fakeVM.equipmentName)
+    }
+
+}
