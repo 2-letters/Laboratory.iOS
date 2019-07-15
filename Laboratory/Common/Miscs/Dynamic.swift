8ecc9ed225fff72462875aabcdc5ac465cceba2f
Huy Vo <voxqhuy@gmail.com>
2019-07-15 19:16:26 -0400

    add MyCache

---
+++ Laboratory/Common/Miscs/Dynamic.swift
@@ -0,0 +1,33 @@
+//
+//  Dynamic.swift
+//  Laboratory
+//
+//  Created by Huy Vo on 7/14/19.
+//  Copyright © 2019 2Letters. All rights reserved.
+//
+
+import Foundation
+
+class Dynamic<T> {
+    typealias Listener = (T) -> ()
+    var listener: Listener?
+    
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+    
+    func bindAndFire(_ listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ v: T) {
+        value = v
+    }
+}
