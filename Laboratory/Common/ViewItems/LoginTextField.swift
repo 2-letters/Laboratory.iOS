737a86cf7319fefa2f133b742a51f557bbfdfbb0
Huy Vo <voxqhuy@gmail.com>
2020-05-10 17:58:08 -0400

    Add Login and sign up view controllers to storyboards

---
+++ Laboratory/Common/ViewItems/LoginTextField.swift
@@ -0,0 +1,25 @@
+//
+//  LoginTextField.swift
+//  Laboratory
+//
+//  Created by Huy Vo on 5/10/20.
+//  Copyright © 2020 2Letters. All rights reserved.
+//
+
+import UIKit
+
+class LoginTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        
+    }
+}
